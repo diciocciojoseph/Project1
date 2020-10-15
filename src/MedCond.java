fc1f966bfd56a962fa09f423fb509611209c115d
ZyongShen <legorye42@gmail.com>
2020-10-15 15:41:07 -0400

    Updated TravProf

--- src/MedCond.java
+++ src/MedCond.java
@@ -1,2 +1,46 @@
 public class MedCond {
+
+    private String mdContact;
+    private String mdPhone;
+    private String algType;
+    private String illType;
+
+    public MedCond(String mdContact, String mdPhone, String algType, String illType) {
+       this.mdContact = mdContact;
+       this.mdPhone = mdPhone;
+       this.algType = algType;
+       this.illType = illType;
+    }
+
+    public String getMdContact() {
+        return this.mdContact;
+    }
+
+    public String getMdPhone() {
+        return this.mdPhone;
+    }
+
+    public String getAlgType() {
+        return this.algType;
+    }
+
+    public String getIllType() {
+        return this.illType;
+    }
+
+    public void updateMdContact(String newMdContact) {
+        this.mdContact = newMdContact;
+    }
+
+    public void updateMdPhone(String newMdPhone) {
+        this.mdPhone = newMdPhone;
+    }
+
+    public void updateAlgType(String newAlgType) {
+        this.algType = newAlgType;
+    }
+
+    public void updateIllType(String newIllType) {
+        this.illType = newIllType;
+    }
 }
